296bed4ed16d6c1bc5752c25caaf4ef5213b3ffc
Favour Chukwuemeka <kaosemeka@yahoo.com>
2025-11-10 13:55:02 +0000

    Disable middleware and exception handlers
    Comment out middleware and exception handling code.

--- src/core/middleware.py
+++ src/core/middleware.py
@@ -1,5 +1,5 @@
 import os, ssl
-from fastapi import FastAPI
+from fastapi import Request, FastAPI
 from starlette.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
@@ -15,28 +15,29 @@ from src.core.dbconfig import (
 )
 
 middlewares = [
-    Middleware(ExceptionMiddleware), 
+    # Middleware(ExceptionMiddleware), 
     Middleware(GZipMiddleware, minimum_size=10), 
     Middleware(TrustedHostMiddleware, allowed_hosts=settings.app_origins), 
-    Middleware(SecurityHeadersMiddleware, csp=True), 
+    # Middleware(SecurityHeadersMiddleware, csp=True), 
     Middleware(CORSMiddleware, allow_origins=settings.app_origins, allow_methods=settings.app_origins, allow_headers=settings.app_origins, allow_credentials=True),
     Middleware(SQLAlchemyMiddleware, db_url=SQLALCHEMY_DATABASE_URL, engine_args=engine_args)
 ]
 
 def add_app_middlewares(app: FastAPI):
-    @app.middleware("http")
-    async def ip_block(request: Request, call_next):
-            return await block_ips(request, call_next)
+    pass
+    # @app.middleware("http")
+    # async def ip_block(request: Request, call_next):
+    #         return await block_ips(request, call_next)
 
-    @app.middleware("http")
-    async def exceptions_middleware(request: Request, call_next):
-        return await catch_exceptions_middleware(request, call_next)
+    # @app.middleware("http")
+    # async def exceptions_middleware(request: Request, call_next):
+    #     return await catch_exceptions_middleware(request, call_next)
 
 async def add_exception_middleware(app: FastAPI):
-    app.add_exception_handler(BaseError, base_error_handler)
-    app.add_exception_handler(BaseError, base_error_handler)
-    app.add_exception_handler(StarletteHTTPException, not_found_handler)
-    app.add_exception_handler(RequestValidationError, validation_exception_handler)
+    pass
+    # app.add_exception_handler(BaseError, base_error_handler)
+    # app.add_exception_handler(StarletteHTTPException, not_found_handler)
+    # app.add_exception_handler(RequestValidationError, validation_exception_handler)
     # app.add_exception_handler(Exception, global_exception_handler)
     
 if os.getenv("SSL") is True:
