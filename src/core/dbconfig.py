84959ba6d40172947a34b28e3641146f328f5be1
Favour Chukwuemeka <kaosemeka@yahoo.com>
2025-11-10 13:55:29 +0000

    Update dbconfig.py

--- src/core/dbconfig.py
+++ src/core/dbconfig.py
@@ -11,6 +11,7 @@ Base: DeclarativeMeta = declarative_base()
 mode = settings.debug
 uri = settings.sqlalchemy_database_uri
 dbname = settings.db_name
+remotedb_url = settings.remotedb_url
 
 SQLALCHEMY_DATABASE_URL = f"{uri}:///" + os.path.join( baseDir, f"{dbname}.db" )
 engine_args: dict = ({})
@@ -38,21 +39,21 @@ async def init_db(app: FastAPI):
 
 async def init_remotedb(app: FastAPI):
     try:
-        app.state.dbengine = create_async_engine(
-            SQLALCHEMY_DATABASE_URL, 
+
+        app.state.remotedbengine = create_async_engine(
+            remotedb_url, 
             echo=False, 
-            connect_args={"check_same_thread": False}, 
             future=True
         )
-        app.state.dbsession = async_sessionmaker(
-            bind=app.state.dbengine,
+        app.state.remotedbsession = async_sessionmaker(
+            bind=app.state.remotedbengine,
             class_=AsyncSession,
             expire_on_commit=False,
             autoflush=False
         )
-        async with app.state.dbengine.begin() as conn:
+        async with app.state.remotedbengine.begin() as conn:
             await conn.run_sync(Base.metadata.create_all)
-            logging.info("✅ Sqlite db initialized successfully")
+            logging.info("✅ Remote db initialized successfully")
     except Exception as e:
         logging.error(str(e))
 
