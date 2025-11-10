bf58219cc25dda49c50d3529eb352c6338d2e7e7
Favour Chukwuemeka <kaosemeka@yahoo.com>
2025-11-10 13:51:59 +0000

    Update user schema to include optional fields

--- src/schemas/user_schema.py
+++ src/schemas/user_schema.py
@@ -5,19 +5,23 @@ from sqlalchemy import Column, Integer, String
 
 
 class IAuth(BaseModel):
-    email: str
-
-    
+    email: Optional[str] = None
+    uid: Optional[str] = None
+    otp: Optional[str] = None
 
 class IUser(BaseModel):
     uid: Optional[str] = None
+    email: Optional[str] = None
+    firstname: Optional[str] = None
+    lastname: Optional[str] = None
 
 
-
-
-class Authentication(Base):
-    __tablename__ = f"authentication"
+class User(Base):
+    __tablename__ = f"useraccounts"
 
     id = Column(Integer, primary_key = True)
     uid = Column(String(100), unique = True)
+    email = Column(String(20))
     otp = Column(String(20))
+    firstname = Column(String(20))
+    lastname = Column(String(20))
